82e208064236d45e409f25f0432f18d74fb7eef4
KhRania <rkhadaier@gmail.com>
2021-03-11 11:38:22 +0100

    update 11/03/2021

--- kioskConfig.sh
+++ kioskConfig.sh
@@ -4,14 +4,23 @@
 # this script should be runned once for set             # 
 # the configuration for kiosk mode                      #
 # and should have exec right                            #
-# $ chmod 777 config.sh                                 #
+# $ sudo chmod 777 config.sh                            #
 #                                                       #
 #########################################################
 
 
+#Disable passwords
+sudo visudo & sed -i 's/%sudo   ALL=(ALL:ALL) ALL/%sudo   ALL=(ALL:ALL) NOPASSWD: ALL/g'
+#disable wayland protocol
+cd /etc/gdm3 
+sed -i 's/#WaylandEnable=false/WaylandEnable=false/g' custom.conf
 #Set the "Blank screen" delay (one hour in our case)
 gsettings set org.gnome.desktop.session idle-delay 3600
 #Disable the lock screen
 gsettings set org.gnome.desktop.lockdown disable-lock-screen true
 # Disable Screen Saver Locking
 gsettings set org.gnome.desktop.screensaver lock-enabled false
+# Disable gnome keyring
+sudo chmod -x /usr/bin/gnome-keyring
+sudo chmod -x /usr/bin/gnome-keyring-daemon
+sudo chmod -x /usr/bin/gnome-keyring-3
